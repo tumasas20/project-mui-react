c309468943139cf23a78958642f891290f3f2acb
tumasas20 <tumasas20@gmail.com>
2023-02-19 12:27:48 +0200

    feat: create badge

--- project with react MUI/src/pages/badge-page.tsx
+++ project with react MUI/src/pages/badge-page.tsx
@@ -1,7 +1,27 @@
 import React from 'react';
+import { Stack, Badge, Container } from '@mui/material';
+import MailIcon from '@mui/icons-material/Mail';
 
 const BadgePage = () => (
-  <div>BadgePage</div>
+  <Container sx={{
+    my: 6, boxShadow: 3, borderRadius: 2, p: 4,
+  }}
+  >
+    <Stack spacing={2} direction="row">
+      <Badge badgeContent={5} color="primary">
+        <MailIcon />
+      </Badge>
+      <Badge badgeContent={0} color="secondary" showZero>
+        <MailIcon />
+      </Badge>
+      <Badge badgeContent={100} color="primary" max={999}>
+        <MailIcon />
+      </Badge>
+      <Badge variant="dot" color="primary" invisible>
+        <MailIcon />
+      </Badge>
+    </Stack>
+  </Container>
 );
 
 export default BadgePage;
