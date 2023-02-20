df69f56170784e94894d85677f668beef4e26227
tumasas20 <tumasas20@gmail.com>
2023-02-20 11:28:42 +0200

    feat: picker page not working

--- project with react MUI/src/pages/date-and-time-picker-page.tsx
+++ project with react MUI/src/pages/date-and-time-picker-page.tsx
@@ -1,7 +1,48 @@
-import React from 'react';
+/* eslint-disable import/no-extraneous-dependencies */
+import React, { useState } from 'react';
+import { Stack, TextField } from '@mui/material';
+import LocalizationProvider from '@mui/lab/LocalizationProvider';
+import { DatePicker, TimePicker, DateTimePicker } from '@mui/lab/';
+import AdaptersDateFns from '@mui/lab/AdapterDateFns';
 
-const DateAndTimePickerPage = () => (
-  <div>DateAndTimePickerPage</div>
-);
+const DateAndTimePickerPage = () => {
+  const [selectDate, setSelectDate] = useState<Date | null>(null);
+  const [selectTime, setSelectTime] = useState<Date | null>(null);
+  const [selectDateTime, setSelectDateTime] = useState<Date | null>(null);
+  console.log({ selectDateTime });
+
+  return (
+    <LocalizationProvider dateAdapter={AdaptersDateFns}>
+
+      <Stack spacing={4} sx={{ width: '250px' }}>
+        <DatePicker
+          label="Date Picker"
+          renderInput={(params) => <TextField {...params} />}
+          value={selectDate}
+          onChange={(newValue) => {
+            setSelectDate(newValue);
+          }}
+        />
+        <TimePicker
+          label="Time Picker"
+          renderInput={(params) => <TextField {...params} />}
+          value={selectTime}
+          onChange={(newValue) => {
+            setSelectTime(newValue);
+          }}
+        />
+        <DateTimePicker
+          label="Date Time Picker"
+          renderInput={(params) => <TextField {...params} />}
+          value={selectDateTime}
+          onChange={(newValue) => {
+            setSelectDateTime(newValue);
+          }}
+        />
+      </Stack>
+
+    </LocalizationProvider>
+  );
+};
 
 export default DateAndTimePickerPage;
